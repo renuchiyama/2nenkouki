9d97cec553c2c2d119c584ef880ae9cdf640f91e
renuchiyama <zm18505@josai.ac.jp>
2019-11-12 03:30:21 +0000

    11.12

---
+++ ex06-6.js
@@ -0,0 +1,13 @@
+var who66 = 'world';
+//ver come ='が来た'
+function sayhello66(){
+//ver who ='world';
+   alert('Hello, '+ who66 + '!') 
+}
+
+function taro66(){
+    who66='太郎';
+}
+function hanako66(){
+    who66='花子';
+}
