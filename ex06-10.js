f86524bc41c31d911aeeb8e39d3316928c48688c
renuchiyama <zm18505@josai.ac.jp>
2019-12-17 03:20:16 +0000

    1217

--- ex06-10.js
+++ ex06-10.js
@@ -12,3 +12,6 @@ function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す　
     setInterval("hyoji()",1000);}
 
 function mes(){alert("3秒経ちました！");}
+
+function imgchange3()
+{document.getElementById('logo').src='./josai2.png';}
